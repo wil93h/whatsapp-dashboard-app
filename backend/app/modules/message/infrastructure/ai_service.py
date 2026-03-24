20f19338ddf9e7d60cb2cc165dd3ac49e6166e5e
rene_mac <rene93h@gmail.com>
2026-03-24 14:48:25 -0600

    Agregar carga de variables de entorno con dotenv en ai_service.py y mongo.py

--- backend/app/modules/message/infrastructure/ai_service.py
+++ backend/app/modules/message/infrastructure/ai_service.py
@@ -1,6 +1,9 @@
 import os
 import json
 import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel('gemini-2.5-flash')
--- backend/app/shared/db/mongo.py
+++ backend/app/shared/db/mongo.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class MongoConnection:
     def __init__(self):
